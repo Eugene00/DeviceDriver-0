ed0091e15055ad860febfcb65f22f11b98a2780f
Seokhwan <sukkakim@gmail.com>
2023-06-05 14:00:13 +0900

    init

---
+++ Sample-Test1/test.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "../DeviceDriver/DeviceDriver.h"
+#include "../DeviceDriver/FlashMemoryDevice.h"
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
